e05396449420b99e4f0f8b644b2af9548ed38474
rockemma <jossuerm55@gmail.com>
2024-05-25 01:01:45 -0600

    Actualización del diseño de formulario

--- components/taskForm/html_form.js
+++ components/taskForm/html_form.js
@@ -15,17 +15,25 @@ const htmlForm = ()=>{
     input_texto.type = "text";
     input_texto.id = "nuevo-item-texto";
     input_texto.placeholder = "Producto";
+    input_texto.autocomplete="off";
+    
 
     const input_numero = document.createElement('input');
     input_numero.type = "number";
     input_numero.id = "nuevo-item-precio";
     input_numero.placeholder = "Q 00.00";
+    input_numero.autocomplete="off";
 
     const button_agregar = document.createElement('button');
     button_agregar.id = "agregar-item";
-    button_agregar.textContent = "Agregar";
+    button_agregar.textContent = "Carrito";
+
+    const button_descuento = document.createElement('button');
+    button_descuento.id = "agregar-descuento";
+    button_descuento.textContent = "Descuento";
 
     div.appendChild(input_texto);
+    div.appendChild(button_descuento);
     div.appendChild(input_numero);
     div.appendChild(button_agregar);
 
