4f72f81d45258e6df6f4a664c8f80e68513a7c43
rockemma <jossuerm55@gmail.com>
2025-04-10 20:07:05 -0600

    reparación de direcdiones

--- sw.js
+++ sw.js
@@ -1,27 +1,9 @@
-// sw.js
-const CACHE_NAME = 'lista-compras-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/main.js',
-  '/assets/favicon/favicon.svg',
-  '/assets/css/config.css',
-  '/components/header/header.css',
-  '/components/task/task.css',
-  '/components/taskForm/taskForm.css',
-  '/components/sectionDownload/download.css'
+  './',  // Usa "./" en lugar de "/"
+  './index.html',
+  './main.js',
+  './assets/favicon/favicon.svg',
+  './assets/css/config.css',
+  './components/header/header.css',
+  './components/task/task.css'
 ];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => res || fetch(e.request))
-  );
-});
